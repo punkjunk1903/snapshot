ba49d49b7a2def7ee8ff0d474dc000f4ff92ba05
EC2 Default User <ec2-user@ip-172-31-19-147.us-west-2.compute.internal>
2018-11-15 15:58:36 +0000

    Some Script Updates

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,33 @@
 import boto3
+import sys
+import click
+
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+
+#Click will execute the Function
+@click.command()
+
+#Function to list all EC2-Insances of an AWS Account
+def list_instances():
+	"List EC2 instances"
+	for i in ec2.instances.all():
+		print(', '.join((
+			i.id,
+			i.instance_type,
+			i.placement['AvailabilityZone'],
+			i.state['Name'],
+			i.public_dns_name)))
+
+	return
+
 
 
 if __name__ == '__main__':
-	session = boto3.Session(profile_name='shotty')
-	ec2 = session.resource('ec2')
-	for i in ec2.instances.all():
-	    print(i)
+	#List the arguments passed to the script
+	print(sys.argv)
+	
+	list_instances()
+
